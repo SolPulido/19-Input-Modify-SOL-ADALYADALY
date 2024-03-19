97c5432145e46325e7df4eb1f06b34642be4fb00
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-03-19 22:24:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+name = "Billy"
+print("We want to know if you like progamming!")
+print()
+print("Do you like programming " + name + "?")
+answer = input()
+print("Great! You said " + answer + "!")
+print("Let's learn some Python today")
